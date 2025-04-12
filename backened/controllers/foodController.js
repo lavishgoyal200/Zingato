03aff12a500830dfd8c12cec957c46b889d88f19
Lavish <goyallavish200@gmail.com>
2025-04-12 19:32:07 +0530

    first commit

--- backened/controllers/foodController.js
+++ backened/controllers/foodController.js
@@ -1,16 +1,15 @@
 import foodModel from "../models/foodModel.js";
 import fs from 'fs';
 
-
 // add food item
 
 const addFood = async (req,res) => {
 
-    let image_filename = await req.file.image;
+    let image_filename = req.file ? req.file.filename : "";
 
     const food = new foodModel({
         name : req.body.name,
-        description : req.file.description,
+        description : req.body.description,
         price : req.body.price,
         category : req.body.category,
         image : image_filename
@@ -20,10 +19,34 @@ const addFood = async (req,res) => {
         res.json({success : true, message : "Food added"})
     }
     catch (error) {
+        console.log(error);
+        res.json({success : false, message : "Error"})
+    }
+}
+
+// all food list
+const listFood =async (req,res) => {
+    try{
+        const food = await foodModel.find({});
+        res.json({success : true, data : food})
+    } catch(error){
         console.log(error)
         res.json({success : false, message : "Error"})
     }
-
 }
 
-export {addFood};
+// remove food item
+const removeFood=async (req,res)=>{
+    try{
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success : true, message : "food Removed "})
+    } catch(error){
+        console.log(error)
+        res.json({success : false, message : "Error"})
+    }
+}
+
+export {addFood,listFood,removeFood};
--- backened/server.js
+++ backened/server.js
@@ -3,6 +3,10 @@ import cors from "cors"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
 
 dotenv.config()
 // app config
@@ -18,6 +22,11 @@ connectDB();
 
 //api endpoints
 app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+
 
 app.get("/",(req,res)=>{
     res.send("API Working")
