bceb684857c2193e5312c310298817a3b6c169c5
Lavish <goyallavish200@gmail.com>
2025-06-30 19:32:26 +0530

    route change

--- Zingato/src/main.jsx
+++ Zingato/src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 import { StoreContextProvider } from './context/storeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  // <HashRouter>
+  <BrowserRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </HashRouter>
+    </BrowserRouter>
+  // </HashRouter>
 )
